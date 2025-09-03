ecd4b83d5a73e0e7bd69e20707647ad5284159cd
fenohasina <fenohasinaherimamy@gmail.com>
2025-09-03 12:01:01 +0300

    envoi de ce de 2em

--- main.py
+++ main.py
@@ -1,8 +1,33 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, status
+from fastapi.responses import JSONResponse
+from pydantic import BaseModel
+from typing import List
 
 app = FastAPI()
 
+ 
+class Characteristic(BaseModel):
+    max_speed: float
+    max_fuel_capacity: float
+
+class Car(BaseModel):
+    identifier: str
+    brand: str
+    model: str
+    characteristics: Characteristic
+
+ 
+CarsStockage: List[Car] = []
+
+ 
 @app.get("/ping")
 def ping():
-    return {"message": "pong"}
+    return "pong"
+
  
+@app.post("/cars", status_code= 201, response_model=Car)
+def create_car(car: Car):
+    CarsStockage.append(car)
+    return car
+
+
